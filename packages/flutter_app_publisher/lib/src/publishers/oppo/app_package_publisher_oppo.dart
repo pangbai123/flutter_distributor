29d00a6434a7f730541f727e74d77630f6d3c6b3
pangbai123 <474923660@qq.com>
2024-05-09 11:55:17 +0800

    12

--- packages/flutter_app_publisher/lib/src/publishers/oppo/app_package_publisher_oppo.dart
+++ packages/flutter_app_publisher/lib/src/publishers/oppo/app_package_publisher_oppo.dart
@@ -5,9 +5,14 @@ import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_app_publisher/src/api/app_package_publisher.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const kEnvClientIdApi = 'OPPO_CLIENT_ID';
 const kEnvAccessSecretApi = 'OPPO_ACCESS_SECRET';
+const kEnvPkgName = 'PKG_NAME';
+const kEnvVersionCode = 'VERSION_CODE';
+const kEnvVersionName = 'VERSION_NAME';
+const kEnvUpdateLog = 'UPDATE_LOG';
 
 ///  doc [https://open.oppomobile.com/new/developmentDoc/info?id=10998]
 class AppPackagePublisherOppo extends AppPackagePublisher {
@@ -15,15 +20,17 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
   String get name => 'oppo';
 
   // dio 网络请求实例
-  final Dio _dio = Dio();
-
-  // 轮询尝试次数
-  int tryCount = 0;
-
-  // 最大尝试轮询次数
-  final maxTryCount = 10;
-  String? client;
+  final Dio _dio = Dio(BaseOptions(
+      connectTimeout: Duration(seconds: 15),
+      receiveTimeout: Duration(seconds: 60)))
+    ..interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+        maxWidth: 600));
+  String? token;
   String? access;
+  late Map<String, String> globalEnvironment;
 
   @override
   Future<PublishResult> publish(
@@ -32,9 +39,10 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
     Map<String, dynamic>? publishArguments,
     PublishProgressCallback? onPublishProgress,
   }) async {
+    globalEnvironment = environment ?? Platform.environment;
     File file = fileSystemEntity as File;
-    client = (environment ?? Platform.environment)[kEnvClientIdApi];
-    access = (environment ?? Platform.environment)[kEnvAccessSecretApi];
+    var client = globalEnvironment[kEnvClientIdApi];
+    access = globalEnvironment[kEnvAccessSecretApi];
     if ((client ?? '').isEmpty) {
       throw PublishError('Missing `$kEnvClientIdApi` environment variable.');
     }
@@ -42,104 +50,126 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
       throw PublishError(
           'Missing `$kEnvAccessSecretApi` environment variable.');
     }
-    Map? map = await getUploadAppUrl(client!, access!);
+    token = await getToken(client!, access!);
+    Map? map = await getUploadAppUrl();
+    print('获取上传地址成功：${jsonEncode(map)}');
     if (map == null) {
       throw PublishError('getUploadAppUrl error');
     }
     String url = map["data"]["upload_url"];
     String sign = map["data"]["sign"];
     Map uploadInfo = await uploadApp(url, sign, file, onPublishProgress);
-
-    //{"errno":0,"data":{"url":"http://storedl1.nearme.com.cn/apk/tmp_apk/202405/08/1829dba19eb6451d9472669d0caac007.apk",
-    // "uri_path":"/apk/tmp_apk/202405/08/1829dba19eb6451d9472669d0caac007.apk","md5":"fc43371e8e57ec2b0fa87c316b52bb89",
-    // "file_size":36028706,"file_extension":"apk","width":0,"height":0,"id":"5db79aa7-5790-4a4a-9188-84b2d455ccd2",
-    // "sign":"49f5785281fa2669c9251152fa73dcb8"},"logid":"5db79aa7-5790-4a4a-9188-84b2d455ccd2"}
-
-    // 重试次数设置为 0
-    tryCount = 0;
-    // var buildResult = await getBuildInfo(apiKey, uploadKey);
-    // String buildKey = buildResult.data!['data']['buildKey'];
+    print('上传文件成功：${jsonEncode(uploadInfo)}');
+    //获取应用信息
+    Map appInfo = await getAppInfo(globalEnvironment[kEnvPkgName]!);
+    print('获取应用信息成功：${jsonEncode(appInfo)}');
+    //提交审核信息
+    Map submitInfo = await submit(uploadInfo, appInfo);
     return PublishResult(
-      url: '${jsonEncode(uploadInfo)}',
+      url: 'oppo提交成功：${jsonEncode(submitInfo)}',
     );
   }
 
-//{
-//         "url": "https://oppo.com/********261d.apk",
-//         "uri_path": "/********261d.apk",
-//         "md5": "5efd****4d4d",
-//         "file_extension": "apk",
-//         "file_size": 4181241,
-//         "id": "XXXXX"
-//     }
-  //
+  Future<Map> submit(Map uploadInfo, Map appInfo) async {
+    Map<String, dynamic> params = appInfo['data'].cast<String, dynamic>();
+
+    params['apk_url'] = [
+      {
+        'url': uploadInfo['data']['url'],
+        'md5': uploadInfo['data']['md5'],
+        'cpu_code': 0,
+      }
+    ];
+    params['version_code'] = globalEnvironment[kEnvVersionCode];
+    params['version_name'] = globalEnvironment[kEnvVersionName];
+    params['update_desc'] = globalEnvironment[kEnvUpdateLog];
+    params['access_token'] = token!;
+    params['timestamp'] = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    String sign = signRequest(access!, params);
+    params['api_sign'] = sign;
+    String content = await sendRequest(
+        'https://oop-openapi-cn.heytapmobi.com/resource/v1/app/upd', params,
+        isGet: false);
+    if (content.isEmpty) {
+      throw PublishError("请求submit失败：$content");
+    }
+    Map map = jsonDecode(content);
+    if (map["errno"] == 0) {
+      return map;
+    } else {
+      throw PublishError("请求submit失败：$content");
+    }
+  }
+
+  ///获取信息
+  Future<Map> getAppInfo(String pkg_name) async {
+    Map<String, dynamic> params = {
+      'access_token': token!,
+      'timestamp': DateTime.now().millisecondsSinceEpoch ~/ 1000,
+      'pkg_name': pkg_name,
+    };
+    String sign = signRequest(access!, params);
+    params['api_sign'] = sign;
+    String content = await sendRequest(
+        'https://oop-openapi-cn.heytapmobi.com/resource/v1/app/info', params);
+    if (content.isEmpty) {
+      throw PublishError("请求getAppInfo失败：$content");
+    }
+    Map map = jsonDecode(content);
+    if (map["errno"] == 0) {
+      return map;
+    } else {
+      throw PublishError("请求getAppInfo失败：$content");
+    }
+  }
+
+  ///上传文件
   Future<Map> uploadApp(
     String url,
     String sign,
     File file,
     PublishProgressCallback? onPublishProgress,
   ) async {
-    List<int> fileBytes = await file.readAsBytes();
-
-    Map<String, dynamic> params = {
-      'access_token': await getToken(client!, access!),
-      'timestamp': DateTime.now().millisecondsSinceEpoch ~/ 1000,
-      'type': 'apk',
-      'sign': sign,
-      "file": fileBytes,
-    };
-
-    String api_sign = signRequest(access!, params);
-    params['api_sign'] = api_sign;
-    String? content = await uploadFile(url, sign, 'apk', file);
-    if (content == null) {
-      throw PublishError('uploadFile erro');
-    }
-    Map responseMap = jsonDecode(content);
-    if (responseMap["errno"] == 0) {
-      return responseMap;
-    } else {
-      throw PublishError(content);
-    }
-  }
-
-  Future<String?> uploadFile(
-      String uploadUrl, String sign, String type, File file) async {
-    var request = http.MultipartRequest('POST', Uri.parse(uploadUrl));
-    request.fields['type'] = type;
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    request.fields['type'] = 'apk';
     request.fields['sign'] = sign;
-    request.fields['access_token'] = await getToken(client!, access!) ?? "";
+    request.fields['access_token'] = token!;
     request.fields['timestamp'] =
         (DateTime.now().millisecondsSinceEpoch ~/ 1000).toString();
     request.files.add(await http.MultipartFile.fromPath('file', file.path));
     var response = await request.send();
     if (response.statusCode == 200) {
       String content = await response.stream.bytesToString();
-      return content;
+      Map responseMap = jsonDecode(content);
+      if (responseMap["errno"] == 0) {
+        return responseMap;
+      } else {
+        throw PublishError(content);
+      }
     } else {
       // 处理错误的响应
       throw PublishError("请求失败：${response.statusCode}");
     }
   }
 
-  Future<Map?> getUploadAppUrl(String client, String secret) async {
+  Future<Map?> getUploadAppUrl() async {
     Map<String, dynamic> params = {
-      'access_token': await getToken(client, secret),
+      'access_token': token,
       'timestamp': DateTime.now().millisecondsSinceEpoch ~/ 1000,
     };
-    String sign = signRequest(secret, params);
+    String sign = signRequest(access!, params);
     params['api_sign'] = sign;
     String content = await sendRequest(
         'https://oop-openapi-cn.heytapmobi.com/resource/v1/upload/get-upload-url',
         params);
     if (content.isEmpty) {
-      throw PublishError("=====请求getUploadAppUrl失败=====");
+      throw PublishError("请求getUploadAppUrl失败：$content");
     }
     Map map = jsonDecode(content);
     if (map["errno"] == 0) {
       return map;
     } else {
-      throw PublishError("=====请求getUploadAppUrl失败=====");
+      throw PublishError("请求getUploadAppUrl失败：$content");
     }
   }
 
@@ -164,63 +194,27 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
     }
   }
 
-  /// 获取应用发布构建信息
-  /// [apiKey] apiKey
-  /// [uploadKey] uploadKey
-  Future<Response> getBuildInfo(String apiKey, String uploadKey) async {
-    if (tryCount > maxTryCount) {
-      throw PublishError('getBuildInfo error :Too many retries');
-    }
-    await Future.delayed(const Duration(seconds: 3));
-    try {
-      Response response = await _dio.get(
-        'https://www.pgyer.com/apiv2/app/buildInfo',
-        queryParameters: {
-          '_api_key': apiKey,
-          'buildKey': uploadKey,
-        },
-      );
-      int code = response.data['code'];
-      if (code == 1247) {
-        tryCount++;
-        print('应用发布信息获取中，请稍等 $tryCount');
-        return await getBuildInfo(apiKey, uploadKey);
-      } else if (code != 0) {
-        throw PublishError('getBuildInfo error: ${response.data}');
-      }
-      return response;
-    } catch (e) {
-      throw PublishError(e.toString());
-    }
-  }
-
   Future<String> sendRequest(String requestUrl, Map<String, dynamic> params,
-      {bool isGet = true}) async {
-    Uri uri = Uri.parse('$requestUrl?${buildQuery(params)}');
+      {Map<String, dynamic>? queryParams, bool isGet = true}) async {
     Response<String> response;
-    if (isGet) {
-      Uri uri = Uri.parse('$requestUrl?${buildQuery(params)}');
-      response = await _dio.get<String>(uri.toString());
-    } else {
-      response = await _dio.post<String>(
-        uri.toString(),
-        data: params,
-      );
+    try {
+      if (isGet) {
+        response = await _dio.get<String>(requestUrl, queryParameters: params);
+      } else {
+        _dio.options.contentType = Headers.formUrlEncodedContentType;
+        response = await _dio.post<String>(requestUrl,
+            data: params, queryParameters: queryParams);
+      }
+    } catch (e) {
+      if (e is DioException) {
+        throw Exception('${e.type} ${e.message}');
+      }
+      throw Exception('${e.toString()} ');
     }
     if ((response.statusCode ?? 0) >= 400) {
       throw Exception('${response.statusCode} ${response.data}');
     }
     return response.data ?? '';
-  }
-
-  String buildQuery(Map<String, dynamic> params, {String charset = 'utf-8'}) {
-    List<String> query = [];
-    params.forEach((key, value) {
-      if (value != null) {
-        query.add('$key=${Uri.encodeQueryComponent(value.toString())}');
-      }
-    });
-    return query.join('&');
   }
 
   String signRequest(String secret, Map<String, dynamic> paramsMap) {
@@ -229,8 +223,7 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
     for (String key in keysList) {
       dynamic object = paramsMap[key];
       if (object == null) continue;
-      String value = '$key=$object';
-      paramList.add(value);
+      paramList.add('$key=$object');
     }
     String signStr = paramList.join('&');
     return hmacSHA256(signStr, secret);
