a6ddb8c97e356484b52ce2652db70d08a2c60494
pangbai123 <474923660@qq.com>
2024-06-07 22:52:49 +0800

    12

--- packages/flutter_app_publisher/lib/src/publishers/doorzo_win/app_package_publisher_doorzo_win.dart
+++ packages/flutter_app_publisher/lib/src/publishers/doorzo_win/app_package_publisher_doorzo_win.dart
@@ -80,8 +80,7 @@ class AppPackagePublisherDoorzoWin extends AppPackagePublisher {
       },
     );
 
-    var key =
-        info[2] + '/' + file.path.substring(file.path.lastIndexOf('/') + 1);
+    var key = info[2] + '/win.exe';
     try {
       var ret = await Client().putObjectFile(
         file.path.replaceAll("+", "%2B"),
