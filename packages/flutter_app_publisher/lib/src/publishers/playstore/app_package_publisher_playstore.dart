6c605179f30a3b9567739d4eb23b4aa7af9a88b0
sw517227087 <517227087@qq.com>
2025-09-01 14:43:30 +0800

    修改三星上架脚本

--- packages/flutter_app_publisher/lib/src/publishers/playstore/app_package_publisher_playstore.dart
+++ packages/flutter_app_publisher/lib/src/publishers/playstore/app_package_publisher_playstore.dart
@@ -9,6 +9,7 @@ import 'package:googleapis_auth/auth_io.dart';
 
 const kEnvTrack = 'GOOGLE_PLAYSTORE_TRACK';
 const kEnvAppName = 'APP_NAME';
+const kEnvReleaseNotes ='UPDATE_RELEASE_NOTES_FILE';
 
 class AppPackagePublisherPlayStore extends AppPackagePublisher {
   @override
@@ -28,8 +29,23 @@ class AppPackagePublisherPlayStore extends AppPackagePublisher {
     PublishProgressCallback? onPublishProgress,
   }) async {
     globalEnvironment = environment ?? Platform.environment;
-    //ceshi
+
     File file = fileSystemEntity as File;
+
+    final releaseNotesList;
+    try{
+      final jsonFile = (environment ?? Platform.environment)[kEnvReleaseNotes];
+      final releaseNotesMap = await loadReleaseNotes(jsonFile);
+      releaseNotesList = releaseNotesMap.entries.map((entry) {
+        return LocalizedText(
+          language: entry.key,
+          text: entry.value,
+        );
+      }).toList();
+    }catch(e){
+      throw PublishError('${globalEnvironment[kEnvAppName]} $name 提交失败: $e');
+    }
+
     try {
       PublishPlayStoreConfig publishConfig = PublishPlayStoreConfig.parse(
         globalEnvironment,
@@ -64,6 +80,7 @@ class AppPackagePublisherPlayStore extends AppPackagePublisher {
         uploadMedia: uploadMedia,
       );
 
+
       if (publishConfig.track != null) {
         final track = Track(
           track: publishConfig.track,
@@ -71,6 +88,7 @@ class AppPackagePublisherPlayStore extends AppPackagePublisher {
             TrackRelease(
               versionCodes: [bundle.versionCode!.toString()],
               status: 'completed',
+              releaseNotes: releaseNotesList
             ),
           ],
         );
@@ -95,5 +113,27 @@ class AppPackagePublisherPlayStore extends AppPackagePublisher {
       print('发布失败: $e\n$stack');
       throw PublishError('${globalEnvironment[kEnvAppName]} $name 提交失败: $e');
     }
+  }
+
+  Future<Map<String, String>> loadReleaseNotes(String? jsonFile) async {
+    if (jsonFile == null || jsonFile.isEmpty) {
+      throw Exception("Release notes JSON file path is not set");
+    }
+
+    final file = File(jsonFile);
+
+    if (!await file.exists()) {
+      throw Exception("Release notes JSON file not found at: $jsonFile");
+    }
+
+    final content = await file.readAsString();
+    final Map<String, dynamic> rawMap = jsonDecode(content);
+
+    // 转成 Map<String, String>
+    final Map<String, String> releaseNotesMap = rawMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return releaseNotesMap;
   }
 }
--- packages/flutter_app_publisher/lib/src/publishers/samsung/app_package_publisher_samsung.dart
+++ packages/flutter_app_publisher/lib/src/publishers/samsung/app_package_publisher_samsung.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter_app_publisher/src/api/app_package_publisher.dart';
-import 'package:flutter_app_publisher/src/publishers/mi/app_package_publisher_mi.dart';
-import 'package:flutter_app_publisher/src/publishers/oppo/app_package_publisher_oppo.dart';
 import 'package:flutter_app_publisher/src/publishers/util.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,22 +10,25 @@ const String serviceAccountId = "SAMSUNG_SACCOUNT_ID";
 const String contentID = "SAMSUNG_CONTENT_ID";
 const String privateKey = "SAMSUNG_PRIVITE_KEY";
 const String baseUrl = "https://devapi.samsungapps.com";
-const String tokenUrl = baseUrl + "/auth/accessToken";
-const String sessionIDUrl = baseUrl + "/seller/createUploadSessionId";
-const String appInfoUrl = baseUrl + "/seller/contentInfo";
-const String updateUrl = baseUrl + "/seller/contentUpdate";
-const String submitUrl = baseUrl + "/seller/contentSubmit";
+const String tokenUrl = "$baseUrl/auth/accessToken";
+const String sessionIDUrl = "$baseUrl/seller/createUploadSessionId";
+const String appInfoUrl = "$baseUrl/seller/contentInfo";
+const String addBinaryUrl = "$baseUrl/seller/v2/content/binary";
+const String modifyBinaryUrl = "$baseUrl/seller/v2/content/binary";
+const String updateUrl = "$baseUrl/seller/contentUpdate";
+const String submitUrl = "$baseUrl/seller/contentSubmit";
 
 
 
 const kEnvPkgName = 'PKG_NAME';
+const kEnvVersionCode = 'VERSION_CODE';
+const kEnvVersionName = 'VERSION_NAME';
+const kEnvAppName = 'APP_NAME';
 
 ///  doc [https://developer.samsung.com/galaxy-store/galaxy-store-developer-api.html]
 class AppPackagePublisherSamsung extends AppPackagePublisher {
   @override
   String get name => 'samsung';
-  String? client;
-  String? access;
   late Map<String, String> globalEnvironment;
   String? accessToken;
   String? uploadUrl;
@@ -35,83 +36,138 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
 
   @override
   Future<PublishResult> publish(
-    FileSystemEntity fileSystemEntity, {
-    Map<String, String>? environment,
-    Map<String, dynamic>? publishArguments,
-    PublishProgressCallback? onPublishProgress,
-  }) async {
+      FileSystemEntity fileSystemEntity, {
+        Map<String, String>? environment,
+        Map<String, dynamic>? publishArguments,
+        PublishProgressCallback? onPublishProgress,
+      }) async {
     globalEnvironment = environment ?? Platform.environment;
+
+    /// Step 1 获取 token
     accessToken = await requestAccessToken();
     if ((accessToken ?? '').isEmpty) {
       throw PublishError('accessToken 为空');
     }
+
+    /// Step 2 创建上传会话
     Map? map = await createUploadSessionId();
     uploadUrl = map?['url'];
     sessionId = map?['sessionId'];
     if ((uploadUrl ?? '').isEmpty || (sessionId ?? '').isEmpty) {
       throw PublishError('uploadUrl 或者 sessionId 为空');
     }
+
+    /// Step 3 获取 App 信息
     Map<String, dynamic>? appInfo = await getAppInfo();
     if (appInfo == null) {
       throw PublishError('app信息 为空');
     }
 
+    /// Step 4 上传 APK
     File file = fileSystemEntity as File;
     Map<String, dynamic>? uploadInfo = await uploadApp(
-            globalEnvironment[kEnvPkgName]!, file, onPublishProgress)
-        as Map<String, dynamic>;
+      globalEnvironment[kEnvPkgName]!,
+      file,
+      onPublishProgress,
+    );
     if (uploadInfo == null) {
       throw PublishError('上传文件信息 为空');
     }
 
-    Map<String, dynamic> binaryParam;
-    List<dynamic> list = appInfo?["binaryList"]??[];
-    // if ((list?.length ?? 0) > 0) {
-    //   binaryParam = Map.from(list![0]);
-    //   binaryParam["binarySeq"] = list?.length.toString();
-    // } else {
-    //   list = [];
-    //   binaryParam = {};
-    //   binaryParam = {"binarySeq":"1","gms": "Y","iapSdk": "N","packageName":globalEnvironment[kEnvPkgName]};
-    // }
-    list = [];
-    binaryParam = {};
-    binaryParam = {"gms": "N","iapSdk": "N","packageName":globalEnvironment[kEnvPkgName]};
-    binaryParam["fileName"] = uploadInfo["fileName"];
-    binaryParam["versionCode"] = globalEnvironment[kEnvVersionCode];
-    binaryParam["versionName"] = globalEnvironment[kEnvVersionName];
-    binaryParam["filekey"] = uploadInfo["fileKey"];
-    list.add(binaryParam);
 
+    /// Step 5 构造 binary 参数
+    Map<String, dynamic> binaryParam = {
+      "gms": "N",
+      "iapSdk": "N",
+      "packageName": globalEnvironment[kEnvPkgName],
+      "fileName": uploadInfo["fileName"],
+      "versionCode": globalEnvironment[kEnvVersionCode],
+      "versionName": globalEnvironment[kEnvVersionName],
+      "filekey": uploadInfo["fileKey"],
+    };
+
+    /// Step 6 调用新增 / 修改二进制接口
+    if ((appInfo["binaryList"] as List?)?.isEmpty ?? true) {
+      await addBinary(appInfo["contentId"], binaryParam);
+    } else {
+      Map<String, dynamic> binaryMap = appInfo["binaryList"][0];
+      // binaryParam['binarySeq'] = binaryMap['binarySeq'];
+      // await deleteBinary(appInfo["contentId"], binaryMap['binarySeq']);
+      await addBinary(appInfo["contentId"], binaryParam);
+      // await modifyBinary(appInfo["contentId"], binaryMap['binarySeq'],binaryParam,);
+    }
+
+    /// Step 7 更新元数据
     Map<String, dynamic> params = {
       "contentId": appInfo["contentId"],
       "appTitle": appInfo["appTitle"],
       "defaultLanguageCode": appInfo["defaultLanguageCode"],
       "paid": appInfo["paid"],
       "publicationType": appInfo["publicationType"],
-      "binaryList": list,
     };
     await updateAppInfo(params);
+
+    /// Step 8 提交审核
     await submit();
-    return PublishResult(url: globalEnvironment[kEnvAppName]! + name + '提交成功}');
+
+    return PublishResult(url: "${globalEnvironment[kEnvAppName]} $name 提交成功");
   }
 
-  submit() {
+  /// ============ API 调用 ============
+
+  Future<void> addBinary(String contentId, Map<String, dynamic> binaryParam) async {
+    Map<String, dynamic> header = getHeaderPrams();
+    binaryParam['contentId'] = contentId;
+    binaryParam['gms'] = "N";
+    Map<String, dynamic> params = binaryParam;
+    Map? response = await PublishUtil.sendRequest(addBinaryUrl, params, header: header, isGet: false);
+    if (response != null && response["errorCode"] != null) {
+      throw PublishError("新增二进制失败: ${response["errorCode"]} ${response["errorMsg"]}");
+    }
+  }
+
+  Future<void> deleteBinary(String contentId, String binarySeq) async {
+    Map<String, dynamic> header = getHeaderPrams();
+    Map<String, dynamic> params = {
+      'contentId':contentId,
+      'binarySeq':binarySeq,
+    };
+    Map? response = await PublishUtil.sendRequest(modifyBinaryUrl, params, header: header,isGet: false, isDelete: true,queryParams:params);
+    if (response != null && response["errorCode"] != null) {
+      throw PublishError("修改二进制失败: ${response["errorCode"]} ${response["errorMsg"]}");
+    }
+  }
+
+  Future<void> modifyBinary(String contentId, String binarySeq,Map<String, dynamic> binaryParam) async {
+    Map<String, dynamic> header = getHeaderPrams();
+    Map<String, dynamic> params = {};
+    binaryParam['contentId'] = contentId;
+    binaryParam['binarySeq'] = binarySeq;
+    binaryParam['gms'] = "N";
+    params = binaryParam;
+    Map? response = await PublishUtil.sendRequest(modifyBinaryUrl, params, header: header,isGet: false, isPut: true);
+    if (response != null && response["errorCode"] != null) {
+      throw PublishError("修改二进制失败: ${response["errorCode"]} ${response["errorMsg"]}");
+    }
+  }
+
+  Future<void> submit() async {
     Map<String, dynamic> header = getHeaderPrams();
     Map<String, dynamic> params = {
       "contentId": globalEnvironment[contentID],
     };
-    dynamic response = PublishUtil.sendRequest(submitUrl, params,
-        header: header, isGet: false);
-    return response;
+    dynamic response = await PublishUtil.sendRequest(submitUrl, params, header: header, isGet: false);
+    if (response != null && response["errorCode"] != null) {
+      throw PublishError("${name} 更新失败: ${response["errorCode"]} ${response["errorMsg"]}");
+    }
   }
 
-  updateAppInfo(Map<String, dynamic> params) async {
+  Future<void> updateAppInfo(Map<String, dynamic> params) async {
     Map<String, dynamic> header = getHeaderPrams();
-    Map? response = await PublishUtil.sendRequest(updateUrl, params,
-        header: header, isGet: false);
-    print("updateAppInfo======${response}========");
-    return;
+    Map? response = await PublishUtil.sendRequest(updateUrl, params, header: header, isGet: false);
+    if (response != null && response["errorCode"] != null) {
+      throw PublishError("${name} 更新失败: ${response["errorCode"]} ${response["errorMsg"]}");
+    }
   }
 
   Future<Map<String, dynamic>?> getAppInfo() async {
@@ -119,8 +175,7 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
     Map<String, dynamic> params = {
       "contentId": globalEnvironment[contentID],
     };
-    dynamic response =
-        await PublishUtil.sendRequest(appInfoUrl, params, header: header);
+    dynamic response = await PublishUtil.sendRequest(appInfoUrl, params, header: header);
     if ((response?.length ?? 0) > 0) {
       return response![0];
     }
@@ -128,62 +183,30 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
   }
 
   Future<Map<String, dynamic>?> uploadApp(
-    String pkg,
-    File file,
-    PublishProgressCallback? onPublishProgress,
-  ) async {
-    // 构建上传的文件
+      String pkg,
+      File file,
+      PublishProgressCallback? onPublishProgress,
+      ) async {
     var request = http.MultipartRequest('POST', Uri.parse(uploadUrl!));
-
-    // 设置请求头
     request.headers.addAll(getHeaderPrams());
-
-    // 将文件添加到请求中
     var fileStream = http.ByteStream(file.openRead());
     var length = await file.length();
-    var multipartFile =
-        http.MultipartFile('file', fileStream, length, filename: "google.apk");
+    var multipartFile = http.MultipartFile('file', fileStream, length, filename: file.path.split("/").last);
     request.files.add(multipartFile);
     request.fields['sessionId'] = sessionId!;
-    if (onPublishProgress != null) {
-      // 你可以在这里通过 ByteStream 和监听进度来实现进度回调
-      // 例如你可以监听 `fileStream`，根据上传进度来触发回调
-    }
 
     try {
-      // 发送请求并等待响应
       var response = await request.send();
       if (response.statusCode == 200) {
-        // 读取响应内容
         var responseData = await response.stream.bytesToString();
-        Map<String, dynamic> responseMap = jsonDecode(responseData);
-        return responseMap; // 或者根据返回格式解析返回的 JSON 数据
+        return jsonDecode(responseData);
       } else {
         print('Upload failed with status: ${response.statusCode}');
       }
     } catch (e) {
       print('Error during file upload: $e');
     }
-
     return null;
-  }
-
-  String generateJWT() {
-    final jwt = JWT(
-      {
-        "iss": globalEnvironment[serviceAccountId],
-        "iat": DateTime.now().millisecondsSinceEpoch ~/ 1000,
-        "scopes": ["publishing", "gss"],
-        "exp":
-            DateTime.now().add(Duration(minutes: 5)).millisecondsSinceEpoch ~/
-                1000,
-      },
-    );
-    final token = jwt.sign(
-      RSAPrivateKey(globalEnvironment[privateKey]!),
-      algorithm: JWTAlgorithm.RS256,
-    );
-    return token;
   }
 
   Future<Map?> createUploadSessionId() async {
@@ -191,35 +214,19 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
     Map<String, dynamic> params = {
       "contentId": globalEnvironment[contentID],
     };
-    Map? response = await PublishUtil.sendRequest(sessionIDUrl, params,
-        header: header, isGet: false);
-    return response;
-  }
-
-  Map<String, String> getHeaderPrams() {
-    return {
-      "Authorization": "Bearer $accessToken",
-      "Content-Type": "application/json",
-      "service-account-id": globalEnvironment[serviceAccountId]!,
-    };
+    return await PublishUtil.sendRequest(sessionIDUrl, params, header: header, isGet: false);
   }
 
   Future<String?> requestAccessToken() async {
     try {
-      final jwt = JWT(
-        {
-          "iss": globalEnvironment[serviceAccountId],
-          "iat": DateTime.now().millisecondsSinceEpoch ~/ 1000,
-          "scopes": ["publishing", "gss"],
-          "exp":
-              DateTime.now().add(Duration(minutes: 5)).millisecondsSinceEpoch ~/
-                  1000,
-        },
-      );
-      final jwtToken = jwt.sign(
-        RSAPrivateKey(globalEnvironment[privateKey]!),
-        algorithm: JWTAlgorithm.RS256,
-      );
+      final jwt = JWT({
+        "iss": globalEnvironment[serviceAccountId],
+        "iat": DateTime.now().millisecondsSinceEpoch ~/ 1000,
+        "scopes": ["publishing", "gss"],
+        "exp": DateTime.now().add(Duration(minutes: 5)).millisecondsSinceEpoch ~/ 1000,
+      });
+      final jwtToken = jwt.sign(RSAPrivateKey(globalEnvironment[privateKey]!), algorithm: JWTAlgorithm.RS256);
+
       final response = await http.post(
         Uri.parse(tokenUrl),
         headers: {
@@ -229,8 +236,7 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
       );
       if (response.statusCode == 200) {
         final responseBody = jsonDecode(response.body);
-        String accessToken = responseBody["createdItem"]?["accessToken"];
-        return accessToken;
+        return responseBody["createdItem"]?["accessToken"];
       } else {
         print("Error: ${response.statusCode} ${response.body}");
         return null;
@@ -239,5 +245,13 @@ class AppPackagePublisherSamsung extends AppPackagePublisher {
       print("Exception: $e");
       return null;
     }
+  }
+
+  Map<String, String> getHeaderPrams() {
+    return {
+      "Authorization": "Bearer $accessToken",
+      "Content-Type": "application/json",
+      "service-account-id": globalEnvironment[serviceAccountId]!,
+    };
   }
 }
